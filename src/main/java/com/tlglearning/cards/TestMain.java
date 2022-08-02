c135d461881e13dc8260cf5f95070a7b2bc243ce
DevHarleyA <aharleydev@gmail.com>
2022-08-02 17:59:03 -0400

    added new pinochle sort method in TestMain

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,34 +2,33 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
+import com.tlglearning.cards.model.Rank;
 import java.security.SecureRandom;
 import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class TestMain {
 
   public static void main(String[] args) {
-    // Create a new deck of cards
     Deck deck = new Deck();
-    // SecureRandom is a subclass of random. It compiles.
     Random rng = new SecureRandom();
-    // invoke toString method to get the string representation of the deck
-    System.out.println(deck);
-    // now let's shuffle the deck and pass our source of randomness
+    Set<Rank> excludedCards = EnumSet.range(Rank.TWO, Rank.EIGHT);
     deck.shuffle(rng);
-    // print it out again
-    System.out.println(deck);
-    // iterate through the cards
-//    for (Card card : deck) {
-//      System.out.println(card);
-//    }
-    deck.sort();
-    System.out.println(deck);
+    Spliterator<Card> splitter = Spliterators.spliterator(deck.iterator(), deck.size(), 0);
+    List<Card> pinochleCards = StreamSupport
+        .stream(splitter, false)
+        .filter((card) -> !excludedCards.contains(card.getRank()))
+        .collect(Collectors.toList());
+    System.out.println(pinochleCards);
 
-    // sort with new RankFirst Comparator
-    deck.sort(Comparator.comparing(Card::getRank).thenComparing(Card::getSuit));
 
-    System.out.println(deck);
 
   }
 
